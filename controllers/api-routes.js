179b0f85c5c02804a912ef83def0d4e6deda420a
Tyler Negro <TylerNegro@tylers-mbp.uconnect.utah.edu>
2017-10-03 21:18:06 -0600

    working on server, login and signup pages and backend

--- controllers/api-routes.js
+++
@@ -1,6 +0,0 @@
-var db = require('../models')
-module.exports = function(app){
-    app.get('/api/high-score', function(req, res){
-        
-    })
-}
--- models/scores.js
+++ models/scores.js
@@ -5,5 +5,7 @@ module.exports = function(sequelize, DataType){
         }
     });
 
+    
+
     return Score;
 }
