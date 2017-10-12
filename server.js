81f8da61c4c2349de29b0146615ab9f02de74905
Tyler Negro <tnegro@gmail.com>
2017-10-11 18:38:38 -0600

    fixed the game-over menu

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var env = require('dotenv').load();
 var exphbs = require('express-handlebars');
-var path = require('path')
+var path = require('path');
 
 var PORT = process.env.PORT || 3000;
 
